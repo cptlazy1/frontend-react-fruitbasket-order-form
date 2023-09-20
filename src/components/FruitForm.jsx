2b3349be952e491674996cb05d996cfe1c8bbca1
cptlazy1 <o.culhaci@gmail.com>
2023-09-20 13:36:21 +0200

    het is klaar

---
+++ src/components/FruitForm.jsx
@@ -0,0 +1,110 @@
+import "./FruitForm.css"
+// eslint-disable-next-line react/prop-types
+function FruitForm({ formState, handleChange, handleSubmit}) {
+    return (
+        <form onSubmit={handleSubmit}>
+            <fieldset className="gegevens-box">
+                <legend><strong>Gegevens</strong></legend>
+                <label htmlFor="first-name">Voornaam: </label>
+                <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    value={formState.firstName}
+                    placeholder="Typ hier je voornaam in"
+                    onChange={handleChange}
+                />
+                <label htmlFor="last-name">Achternaam: </label>
+                <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    value={formState.lastName}
+                    placeholder="Typ hier je naam in"
+                    onChange={handleChange}
+                />
+                <label htmlFor="leeftijd">Leeftijd: </label>
+                <input
+                    type="number" //
+                    id="age"
+                    name="age"
+                    value={formState.age}
+                    placeholder="0"
+                    onChange={handleChange}
+                />
+                <label htmlFor="postcode">Postcode: </label>
+                <input
+                    type="text"
+                    id="postcode"
+                    name="postcode"
+                    value={formState.postcode}
+                    placeholder="1234AB"
+                    onChange={handleChange}
+                />
+            </fieldset>
+
+            <h3>Bezorgfrequentie</h3>
+
+            <select
+                id="deliveryFrequency"
+                name="deliveryFrequency"
+                value={formState.deliveryFrequency}
+                onChange={handleChange}
+            >
+                <option value="iedere week">Iedere week</option>
+                <option value="om de week">Om de week</option>
+                <option value="iedere maand">Iedere maand</option>
+            </select>
+
+
+            <input
+                type="radio"
+                id="deliveryTimeOverdag"
+                name="deliveryTime"
+                value="overdag"
+                checked={formState.deliveryTime === "overdag"}
+                onChange={handleChange}
+            />
+            <label>Overdag</label>
+
+            <input
+                type="radio"
+                id="deliveryTimeAvonds"
+                name="deliveryTime"
+                value="'s avonds"
+                checked={formState.deliveryTime === "'s avonds"}
+                onChange={handleChange}
+            />
+            <label>'s Avonds</label>
+            <br />
+            <br />
+            <label>Opmerking</label>
+            <br />
+            <textarea
+                name="remarks"
+                id="remarks"
+                rows="5"
+                cols="45"
+                placeholder="Laat hier je opmerkingen achter"
+                value={formState.remarks}
+                onChange={handleChange}
+            ></textarea>
+            <br />
+            <input
+                type="checkbox"
+                id="agreeTermsAndConditions"
+                name="agreeTermsAndConditions"
+                checked={formState.agreeTermsAndConditions}
+                onChange={handleChange}
+            />
+            <label>Ik ga akkoord met de voorwaarden</label>
+            <br />
+            <br />
+            <input
+                type="submit"
+                value="Verzenden"
+            />
+        </form>
+    );
+}
+export default FruitForm
